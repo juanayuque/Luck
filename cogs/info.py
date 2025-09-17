09102cb25749ad098700daf18fd7fb2b53aaf392
Juan <147558142+juanayuque@users.noreply.github.com>
2025-09-17 11:45:28 +0100

    bot

--- cogs/info.py
+++ cogs/info.py
@@ -1,4 +1,6 @@
-from discord import app_commands  
+import discord                      
+from discord import app_commands   
+from discord.ext import commands
 import requests
 import re
 import io
